43e1c3a96d325834e340cd108cbb97214916730a
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-11-27 12:31:04 +0100

    DBContext Setup

--- CrownCleanApp.Infrastructure.Data/CrownCleanAppContext.cs
+++ CrownCleanApp.Infrastructure.Data/CrownCleanAppContext.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using CrownCleanApp.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CrownCleanApp.Infrastructure.Data
 {
-    public class CrownCleanAppContext
+    public class CrownCleanAppContext : DbContext
     {
+        public CrownCleanAppContext(DbContextOptions<CrownCleanAppContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Orders)
+                .WithOne(o => o.User)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Vehicles)
+                .WithOne(v => v.User)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
     }
 }
