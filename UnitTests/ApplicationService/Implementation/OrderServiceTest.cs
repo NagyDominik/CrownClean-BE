485d3eb87c20b7828aa445cbd7611fc6965d7e11
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-11-28 11:54:45 +0100

    Split test classes

---
+++ UnitTests/ApplicationService/Implementation/OrderServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Moq;
+using CrownCleanApp.Core.ApplicationService;
+using CrownCleanApp.Core.ApplicationService.Services;
+using CrownCleanApp.Core.DomainService;
+using CrownCleanApp.Core.Entity;
+using System.Collections;
+using System;
+
+namespace TestCore.ApplicationService.Implementation
+{
+    public class OrderServiceTest
+    {
+       
+    }
+}
